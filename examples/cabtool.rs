39da2629e9e45dbd590657a453e161792ef7fd74
Ivan Krivosheev <py.krivosheev@gmail.com>
2023-03-25 13:21:15 +0300

    feat: Upgrade edition and examples

--- examples/cabtool.rs
+++ examples/cabtool.rs
@@ -1,96 +1,79 @@
-use cab::{Cabinet, CabinetBuilder, CompressionType, FileEntry, FolderEntry};
-use clap::{App, Arg, SubCommand};
 use std::fs::{self, File};
 use std::io;
 use std::path::PathBuf;
 use std::time::UNIX_EPOCH;
+
+use clap::{Parser, Subcommand};
 use time::{OffsetDateTime, PrimitiveDateTime};
 
-// ========================================================================= //
+use cab::{Cabinet, CabinetBuilder, CompressionType, FileEntry, FolderEntry};
+
+#[derive(Parser, Debug)]
+#[command(author, about, version)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Concatenates and prints streams
+    Cat { path: PathBuf, files: Vec<String> },
+    /// Creates a new cabinet
+    Create {
+        /// Sets compression type
+        #[clap(short, long, default_value_t = String::from("mszip"))]
+        compress: String,
+        /// Sets output path
+        #[clap(short, long)]
+        output: Option<PathBuf>,
+        files: Vec<String>,
+    },
+    /// Lists files in the cabinet
+    Ls {
+        /// Lists in long format
+        #[clap(short, long)]
+        long: bool,
+        path: PathBuf,
+    },
+}
 
 fn main() {
-    let matches = App::new("cabtool")
-        .version("0.1")
-        .author("Matthew D. Steele <mdsteele@alum.mit.edu>")
-        .about("Manipulates CAB files")
-        .subcommand(
-            SubCommand::with_name("cat")
-                .about("Concatenates and prints streams")
-                .arg(Arg::with_name("cab").required(true))
-                .arg(Arg::with_name("file").multiple(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("create")
-                .about("Creates a new cabinet")
-                .arg(
-                    Arg::with_name("compress")
-                        .takes_value(true)
-                        .value_name("TYPE")
-                        .short("c")
-                        .long("compress")
-                        .help("Sets compression type"),
-                )
-                .arg(
-                    Arg::with_name("output")
-                        .takes_value(true)
-                        .value_name("PATH")
-                        .short("o")
-                        .long("output")
-                        .help("Sets output path"),
-                )
-                .arg(Arg::with_name("file").multiple(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("ls")
-                .about("Lists files in the cabinet")
-                .arg(
-                    Arg::with_name("long")
-                        .short("l")
-                        .help("Lists in long format"),
-                )
-                .arg(Arg::with_name("cab").required(true)),
-        )
-        .get_matches();
-    if let Some(submatches) = matches.subcommand_matches("cat") {
-        let mut cabinet =
-            open_cab(submatches.value_of("cab").unwrap()).unwrap();
-        if let Some(filenames) = submatches.values_of("file") {
-            for filename in filenames {
-                let mut file_reader = cabinet.read_file(filename).unwrap();
+    let cli = Cli::parse();
+    match cli.command {
+        Command::Cat { path, files } => {
+            let mut cabinet = Cabinet::new(File::open(path).unwrap()).unwrap();
+            for filename in files {
+                let mut file_reader = cabinet.read_file(&filename).unwrap();
                 io::copy(&mut file_reader, &mut io::stdout()).unwrap();
             }
         }
-    } else if let Some(submatches) = matches.subcommand_matches("create") {
-        let ctype = if let Some(string) = submatches.value_of("compress") {
-            let string = string.to_lowercase();
-            match string.as_str() {
+        Command::Create { compress, output, files } => {
+            let compress = match compress.as_str() {
                 "none" => CompressionType::None,
                 "mszip" => CompressionType::MsZip,
-                _ => panic!("Invalid compression type: {}", string),
-            }
-        } else {
-            CompressionType::MsZip
-        };
-        let out_path = if let Some(path) = submatches.value_of("output") {
-            PathBuf::from(path)
-        } else {
-            let mut path = PathBuf::from("out.cab");
-            let mut index: i32 = 0;
-            while path.exists() {
-                index += 1;
-                path = PathBuf::from(format!("out{}.cab", index));
-            }
-            path
-        };
-        let mut builder = CabinetBuilder::new();
-        if let Some(filenames) = submatches.values_of("file") {
-            let filenames: Vec<&str> = filenames.collect();
+                _ => panic!("Invalid compression type: {}", compress),
+            };
+
+            let output = match output {
+                Some(out) => out,
+                None => {
+                    let mut path = PathBuf::from("out.cab");
+                    let mut index: i32 = 0;
+                    while path.exists() {
+                        index += 1;
+                        path = PathBuf::from(format!("out{}.cab", index));
+                    }
+                    path
+                }
+            };
+            let mut builder = CabinetBuilder::new();
             let mut file_index: usize = 0;
-            while file_index < filenames.len() {
-                let folder = builder.add_folder(ctype);
+            while file_index < files.len() {
+                let folder = builder.add_folder(compress);
                 let mut folder_size: u64 = 0;
-                while file_index < filenames.len() && folder_size < 0x8000 {
-                    let filename = filenames[file_index];
+                while file_index < files.len() && folder_size < 0x8000 {
+                    let filename = files[file_index].as_str();
                     let metadata = fs::metadata(filename).unwrap();
                     folder_size += metadata.len();
                     let file = folder.add_file(filename);
@@ -109,26 +92,24 @@ fn main() {
                     file_index += 1;
                 }
             }
+            let file = File::create(&output).unwrap();
+            let mut cabinet = builder.build(file).unwrap();
+            while let Some(mut writer) = cabinet.next_file().unwrap() {
+                let mut file = File::open(writer.file_name()).unwrap();
+                io::copy(&mut file, &mut writer).unwrap();
+            }
+            cabinet.finish().unwrap();
         }
-        let file = File::create(&out_path).unwrap();
-        let mut cabinet = builder.build(file).unwrap();
-        while let Some(mut writer) = cabinet.next_file().unwrap() {
-            let mut file = File::open(writer.file_name()).unwrap();
-            io::copy(&mut file, &mut writer).unwrap();
-        }
-        cabinet.finish().unwrap();
-    } else if let Some(submatches) = matches.subcommand_matches("ls") {
-        let long = submatches.is_present("long");
-        let cabinet = open_cab(submatches.value_of("cab").unwrap()).unwrap();
-        for (index, folder) in cabinet.folder_entries().enumerate() {
-            for file in folder.file_entries() {
-                list_file(index, folder, file, long);
+        Command::Ls { path, long } => {
+            let cabinet = Cabinet::new(File::open(path).unwrap()).unwrap();
+            for (index, folder) in cabinet.folder_entries().enumerate() {
+                for file in folder.file_entries() {
+                    list_file(index, folder, file, long);
+                }
             }
         }
     }
 }
-
-// ========================================================================= //
 
 fn list_file(
     folder_index: usize,
@@ -170,9 +151,3 @@ fn list_file(
         file.name()
     );
 }
-
-fn open_cab(path: &str) -> io::Result<Cabinet<File>> {
-    Cabinet::new(File::open(path)?)
-}
-
-// ========================================================================= //
